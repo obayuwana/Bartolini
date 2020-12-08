411b176fe5198c9dda45fdb3d510b0901785df99
obayuwana <73296673+obayuwana@users.noreply.github.com>
2020-12-08 19:13:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $('.carousel').carousel({
   interval: false
 })
+
+if ($(window).width() > 992) {
+  $(window).scroll(function(){
+     if ($(this).scrollTop() > 40) {
+        $('#head').addClass("fixed-top");
+        $('body').css('padding-top', $('.navbar').outerHeight() + 'px');}
+        else{
+        $('#head').removeClass("fixed-top");
+        $('body').css('padding-top', '0');}
+  });
+}
